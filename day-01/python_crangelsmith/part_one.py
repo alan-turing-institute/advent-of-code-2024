67ca45913fd6f89ccbb2b6bea9b5080831c376f2
crangelsmith <crangelsmith@turing.ac.uk>
2024-12-01 10:00:49 +0000

    day1 Camila fix

--- day-01/python_crangelsmith/part_one.py
+++ day-01/python_crangelsmith/part_one.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('advent_of_code.csv')
+df = pd.read_csv('advent_of_code.csv',header=None, names=['list1', 'list2'])
 
 sorted_list1 = sorted(df['list1'].to_list())
 sorted_list2 = sorted(df['list2'].to_list())
--- day-01/python_crangelsmith/part_two.py
+++ day-01/python_crangelsmith/part_two.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('advent_of_code.csv')
+df = pd.read_csv('advent_of_code.csv',header=None, names=['list1', 'list2'])
 
 list1 = df['list1'].to_list()
 total_list = [df[df['list2'] == val].shape[0] * val for val in list1]
