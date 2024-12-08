1d9d1bf43097fcdc32681f87a9bd2b52607cecb4
Fiona Young <fiona.young.15@ucl.ac.uk>
2024-12-08 13:49:35 +0000

    Day 8, part 2 (python)

--- day-08/python_fionaEyoung/solution.py
+++ day-08/python_fionaEyoung/solution.py
@@ -1,15 +1,21 @@
 import numpy as np
 from itertools import combinations
 
-def find_antinodes(map, result):
+def find_antinodes(map, result, result_with_harmonics):
   global FLAG
   antennas = np.argwhere(map)
 
   for a, b in combinations(antennas, 2):
-    for pos in [2*b - a, 2*a - b]:
+    dir = b - a
+    min_harmonic = max(a//abs(dir))
+    max_harmonic = max((map.shape-a)//abs(dir))
+    for harmonic in range(-min_harmonic, max_harmonic+1):
+      pos = a + harmonic*(b-a)
       if any(pos<0): continue
       try:
-        result[tuple(pos)] = True
+        if harmonic in (-1, 2): # Part 1 only
+          result[tuple(pos)] = True
+        result_with_harmonics[tuple(pos)] = True
       except IndexError:
         continue
 
@@ -26,12 +32,14 @@ def main():
   int_map = np.reshape(int_map, char_map.shape)
 
   antinodes = np.zeros(char_map.shape, dtype=bool)
+  antinodes_with_harmonics = np.zeros(char_map.shape, dtype=bool)
 
   for i, f in enumerate(freqs):
     if f == '.': continue
-    find_antinodes(int_map==i, antinodes)
+    find_antinodes(int_map==i, antinodes, antinodes_with_harmonics)
 
   print("Part 1: ", antinodes.sum())
+  print("Part 2: ", antinodes_with_harmonics.sum())
 
 if __name__ == '__main__':
   main()
