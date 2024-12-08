934427aae67cd3267dc5e30cf908f12c574c243e
David Barragan <dbarraganalcantar@turing.ac.uk>
2024-12-08 10:36:11 +0000

    day-08 solution

--- day-08/python_itsdaveba/solution.py
+++ day-08/python_itsdaveba/solution.py
@@ -1,5 +1,5 @@
 import numpy as np
-from itertools import product, combinations
+from itertools import product, permutations
 from collections import defaultdict
 
 with open("input.txt") as file:
@@ -35,13 +35,12 @@ print("Part One:", answer)
 
 # we already have the antennas locations
 for locations in antennas.values():
-    for loc1, loc2 in combinations(locations, r=2):
-        directions = [np.subtract(loc1, loc2), np.subtract(loc2, loc1)]
-        antinode_locs = map(np.array, [loc1, loc2])
-        for direction, antinode_loc in zip(directions, antinode_locs):
-            while np.all((antinode_loc >= 0) & (antinode_loc < city_map.shape)):
-                antinode_map[tuple(antinode_loc)] = True
-                antinode_loc += direction
+    for loc1, loc2 in permutations(locations, r=2):
+        direction = np.subtract(loc1, loc2)
+        antinode_loc = np.array(loc1)
+        while np.all((antinode_loc >= 0) & (antinode_loc < city_map.shape)):
+            antinode_map[tuple(antinode_loc)] = True
+            antinode_loc += direction
 
 answer = antinode_map.sum()
 print("Part Two:", answer)
