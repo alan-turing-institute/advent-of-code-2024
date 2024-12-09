18ec8c2aac2f572fdb5aad1a1988fa03882e01cb
David Barragan <dbarraganalcantar@turing.ac.uk>
2024-12-09 12:38:15 +0000

    day-09 solution

---
+++ day-09/python_itsdaveba/solution.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+with open("input.txt") as file:
+    input = file.read()
+
+disk_map = list(map(int, input))
+FREE_SPACE = -1
+
+# PART ONE
+
+layout = []
+for i, digit in enumerate(disk_map):
+    if i % 2 == 0:
+        id_number = i // 2
+        layout.extend([id_number] * digit)
+    else:
+        layout.extend([FREE_SPACE] * digit)
+
+new_layout = layout.copy()
+left, right = 0, len(layout) - 1  # two pointers
+while left < right:
+    if new_layout[left] == FREE_SPACE:
+        new_layout[left] = new_layout[right]  # assume the disk_map ends with a file
+        right -= 1
+        while new_layout[right] == FREE_SPACE:
+            right -= 1
+    left += 1
+
+checksum = 0
+for i, id_number in enumerate(new_layout[: right + 1]):
+    checksum += i * id_number
+
+print("Part One:", checksum)
+
+
+# PART TWO
+
+# now we need to check `disk_map` because we are moving complete files
+starting_block = np.cumsum(disk_map)
+file_blocks = [(int(index), num_blocks)for index, num_blocks in zip(starting_block[1::2], disk_map[2::2])]  # index, num_blocks
+free_blocks = [(int(index), num_blocks) for index, num_blocks in zip(starting_block[0::2], disk_map[1::2])]  # index, num_blocks
+new_layout = np.array(layout)
+while file_blocks:
+    file_index, file_size = file_blocks.pop()
+    for i, (index, num_blocks) in enumerate(free_blocks):
+        if index >= file_index:
+            break
+        if num_blocks >= file_size:
+            new_layout[index: index + file_size] = new_layout[file_index: file_index + file_size]
+            new_layout[file_index: file_index + file_size] = FREE_SPACE
+            free_blocks[i] = (index + file_size, num_blocks - file_size)
+            break
+
+checksum = 0
+for i, id_number in enumerate(new_layout):
+    if id_number == FREE_SPACE:
+        continue
+    checksum += i * id_number
+
+print("Part Two:", checksum)
