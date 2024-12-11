7b8f0981ff55c4f5a3ec2f07157f4d6a3fad00e0
crangelsmith <crangelsmith@turing.ac.uk>
2024-12-11 22:25:18 +0000

    day 11

--- day-11/python_crangelsmith/day11.py
+++ day-11/python_crangelsmith/day11.py
@@ -9,61 +9,49 @@ def parse_file(file_path, delimiter=" "):
     return [list(map(int, l.split(delimiter))) for l in lines][0]
 
 
-def rules(value, count, temp):
+def rules(value, count, data_dict):
     if value == 0:
-        temp[1] += count
+        data_dict[1] += count
     else:
         log_val = int(math.log10(value) + 1)
         if (log_val % 2) == 0:
             first_half = value // 10 ** (log_val // 2)
             second_half = value % 10 ** (log_val // 2)
-            temp[first_half] += count
-            temp[second_half] += count
+            data_dict[first_half] += count
+            data_dict[second_half] += count
         else:
-            temp[value * 2024] += count
+            data_dict[value * 2024] += count
 
-    return temp
+    return data_dict
 
 
-
-def blink_n_times(d_temp, n):
+def blink_n_times(data, n):
     output = defaultdict(int)
-    for value, count in d_temp.items():
+    for value, count in data.items():
         output = rules(value, count, output)
-    d_temp = output.copy()
+    data = output.copy()
 
     n = n - 1
 
     if n == 0:
-        return d_temp
+        return data
     else:
-        return blink_n_times(d_temp, n)
-
-
-
-
+        return blink_n_times(data, n)
 
 
 if __name__ == "__main__":
 
-    import time
-
-    time_start1 = time.time()
     file_name = "input.txt"
-
     data = parse_file(file_name)
-
 
     n = 75
     total = 0
     for i in sorted(data):
-        temp_i = defaultdict(int)
-        temp_i[i] += 1
-        temp_i  = blink_n_times(temp_i, n)
+        result_i = defaultdict(int)
+        result_i[i] += 1
+        result_i = blink_n_times(result_i, n)
 
-        for c in temp_i.values():
-            total += c
+        for value in result_i.values():
+            total += value
 
     print("Part 2: ", total)
-
-
