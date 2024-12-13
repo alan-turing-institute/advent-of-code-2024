93f236e2987a112dceb8a471e5c2e4012f9a66dd
Fiona Young <fyoung@turing.ac.uk>
2024-12-13 15:37:48 +0000

    Day 13 part 2 (python)

--- day-13/python_fionaEyoung/solution.py
+++ day-13/python_fionaEyoung/solution.py
@@ -30,3 +30,28 @@ for m in machines:
         break
 
 print("Part 1: ", total_cost)
+
+## Part 2
+
+# Modify Ps
+for m in machines:
+    m['P'] += 10000000000000
+
+def solve_claw(A, B, P):
+    # Linear equations baby
+    n_B = ( P[1]*A[0] - P[0]*A[1] ) / ( B[1]*A[0] - B[0]*A[1] )
+    n_A = ( P[0] - n_B*B[0]) / A[0]
+    if not bool(n_B % 1) and not bool(n_A % 1):
+        return int(n_A), int(n_B)
+    else:
+        return 0, 0
+
+total = 0
+for m in machines:
+    n_A, n_B = solve_claw(m['A'], m['B'], m['P'])
+    total += n_B*cost_B + n_A*cost_A
+
+if fname=='test.txt':
+    assert total==875318608908
+
+print("Part 1: ", total)
