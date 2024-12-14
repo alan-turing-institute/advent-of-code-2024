5e4e78a7f09811019f3661f1463dba46db71ee72
David Barragan <dbarraganalcantar@turing.ac.uk>
2024-12-14 20:07:27 +0000

    day-14 solution

--- day-14/python_itsdaveba/solution.py
+++ day-14/python_itsdaveba/solution.py
@@ -7,17 +7,6 @@ with open("input.txt") as file:
 TIME = 100
 SPACE = np.array((101, 103))
 
-# get to the bathroom
-# swarmed with robots
-# robots move in straight lines
-# position and velocities
-# 101 tiles wide and 103 tiles tall
-# test 11 tiles wide and 7 tiles tall
-# they can teleport
-# position after 100 seconds
-# count the number of robots in each quadrant after 100 seconds
-# Multiplying these together gives a total safety factor
-
 pattern = r"-?\d+,-?\d+"
 p = re.compile(pattern)
 
@@ -29,19 +18,6 @@ for line in input.splitlines():
 
 positions = np.array(positions)
 velocities = np.array(velocities)
-
-# Part Two
-# with open("output.txt", "w") as file:
-#     for t in range(7503):
-#         space_map = np.zeros(SPACE, dtype=bool)
-#         space_map[positions[:, 0], positions[:, 1]] = True
-#         file.write(str(t) + "\n")
-#         for row in space_map.T:
-#             for col in row:
-#                 file.write("#" if col else ".")
-#             file.write("\n")
-#         positions += velocities
-#         positions %= SPACE
 
 positions += velocities * TIME
 positions %= SPACE
