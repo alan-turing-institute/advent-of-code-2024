40c943f40bc0f89dfcc03156c49b108d63296060
James Geddes <jgeddes@turing.ac.uk>
2024-12-17 12:06:14 +0000

    Working on part 2

--- day-17/racket_triangle-man/day17.rkt
+++ day-17/racket_triangle-man/day17.rkt
@@ -3,13 +3,44 @@
 (module+ main
 
   ;; Part 1
+
+  (define-values (*state* *memory*)
+    (values (<state> 64751475 0 0 0)
+            #(2 4 1 2 7 5 4 5 1 3 5 5 0 3 3 0)))
   
-  (machine/run (<state> 64751475 0 0 0)
-               #(2 4 1 2 7 5 4 5 1 3 5 5 0 3 3 0))
+  (machine/run *state* *memory*)
 
   ;; Part 2
+
+  ;; Oh, this doesn't work
+  ;; (search-for-quine #(2 4 1 2 7 5 4 5 1 3 5 5 0 3 3 0))  
+
+  ;; .start
+  ;; 2 4 | B <- (A mod 8)         ; B is lowest three bits of A
+  ;; 1 2 | B <- B xor b010        ; flip bit 2 of B
+  ;; 7 5 | C <- A >> B            ; C is A >> B (anywhere from 0 to 3 shift) 
+  ;; 4 5 | B <- B xor C           ; flip C bits of B
+  ;; 1 3 | B <- B xor b011        ; flip bits 1 and 2 of B
+  ;; 5 5 | out (B mod 8)          ; output the lowest three bits of B
+  ;; 0 3 | A <- A >> 3            ; get the next three bits of A
+  ;; 3 0 | jump (ne? A 0) .start  ; and repeat
+
+  (define (find-A-to-produce val start memory)
+    (for/or ([A (in-range start (+ start 8))])
+      (let ([output
+             (machine/run (<state> A 0 0 0) memory)])
+        (and (equal? (car output) val)
+             A))))
+
+  (machine/run (<state> 4329 0 0 0) *memory*)
   
-  (search-for-quine #(2 4 1 2 7 5 4 5 1 3 5 5 0 3 3 0))  
+  (for/fold ([A 0])
+            ([op (reverse (vector->list *memory*))])
+    (let ([next-A 
+           (find-A-to-produce op A *memory*)])
+      (displayln next-A)
+      (arithmetic-shift next-A 3)))
+
   
   )
 
@@ -92,15 +123,15 @@
           (loop (machine/step state memory))))))
 
 ;; For part 2
-(define (search-for-quine memory)
-  (let ([original-program (vector->list memory)])
-    (for ([A (in-naturals)])
-      (when (zero? (modulo A 1048576)) (displayln (format "~a " A)))
-      (when (equal? original-program
-                    (machine/run (<state> A 0 0 0) memory))
-        (displayln (format "Found it! At A = ~a")))
-      )
-    ))
+;; (define (search-for-quine memory)
+;;   (let ([original-program (reverse (vector->list memory))])
+;;     (for ([A (in-naturals 161480704)])
+;;       (when (zero? (modulo A 1048576)) (displayln (format "~a " A)))
+;;       (when (equal? original-program
+;;                     (machine/run (<state> A 0 0 0) memory))
+;;         (displayln (format "Found it! At A = ~a" A)))
+;;       )
+;;     ))
   
 
   ;; ------------------------------------------------------------------------------------------
@@ -120,5 +151,11 @@
   
   ;; B should be 44354
   (machine/run (<state> 0 2024 43690 0) #(4 0))
+
+  ;; Part 2
+
+  (machine/run (<state> 117440 0 0 0) #(0 3 5 4 3 0))
+  
+  ;; (search-for-quine #(0 3 5 4 3 0))
   )
 
