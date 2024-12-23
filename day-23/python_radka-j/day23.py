55e7394b29a30d027e24b896f40eb0e02caf22df
radka-j <r.jersakova@gmail.com>
2024-12-23 23:05:10 +0100

    part 2

--- day-23/python_radka-j/day23.py
+++ day-23/python_radka-j/day23.py
@@ -23,3 +23,39 @@ for node in graph:
                     connections.append(tuple(sorted((node, node2, node3))))
 
 print(len(set(connections)))
+
+# PART 2: https://en.wikipedia.org/wiki/Bron–Kerbosch_algorithm
+#
+# To start, set R and X to be the empty set and P to be the set of all nodes in the graph
+# then consider each node in P one at a time (N[v] are the neighbours of node v)
+#
+# algorithm BronKerbosch(R, P, X) is
+#     if P and X are both empty then
+#         report R as a maximal clique
+#     for each vertex v in P do
+#         BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+#         P := P \ {v}
+#         X := X ⋃ {v}
+
+
+def bronkerbosch(R, P, X):
+    if len(P) == 0 and len(X) == 0:
+        return [R]
+    cliques = []
+    for node in list(P):
+        neighbours = set(graph[node])
+        cliques += bronkerbosch(
+            set(R).union({node}),
+            set(P).intersection(neighbours),
+            set(X).intersection(neighbours),
+        )
+        X.add(node)
+        P.remove(node)
+    return cliques
+
+
+all_cliques = bronkerbosch(set(), set(graph.keys()), set())
+for clique in all_cliques:
+    # from looking at the output, know that the biggest clique has length 13
+    if len(clique) > 12:
+        print(sorted(clique))
